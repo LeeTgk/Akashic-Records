19f8554b521a377978f7296dfd187670160207ff
Paulo Vítor <liborio@aluno.puc-rio.br>
2021-11-27 22:19:36 -0300

    add menv

--- Gpt2Integration/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class Gpt2IntegrationConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Gpt2Integration'
---
+++ akashicrecords/gpt2integration/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Gpt2IntegrationConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'gpt2integration'
